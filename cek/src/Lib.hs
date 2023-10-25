9f7d6d40abb6543071473928e0b1ae6eb41ee483
Reza <rezahandzalah@gmail.com>
2023-10-25 08:55:51 +0700

    manual tests ok

--- cek/src/Lib.hs
+++ cek/src/Lib.hs
@@ -21,33 +21,40 @@ data Kont = Mt
 type Var = String
 
 data D = Clo (Lambda, Env) deriving Show
-defaultD = Clo (c0, defaultEnv)
+defaultD = Clo (asChurchNum 0, defaultEnv)
 
 data Lambda = Var :=> Exp
         deriving Show
 
--- mod lambda calc codec begin
-ident = "x" :=> Ref "x"
-c0    = "f" :=> Lam ident
-fx    = (Ref "f") :@ (Ref "x")
-c1    = "f" :=> Lam ("x" :=> fx)
 
+-- CHURCH ENCODING
+
+ident = "x" :=> Ref "x"
+
+asChurchNum :: Int -> Lambda
+asChurchNum n = "f" :=> Lam ("x" :=> (tally n)) where tally :: Int -> Exp
+                                                      tally 0 = Ref "x"
+                                                      tally n = (Ref "f") :@ (tally $ pred n)
+
+asNat :: Exp -> Int
 asNat e = h e 0 where
         h (Ref _) x          = x
         h (Lam (_ :=> t)) x  = h t x
         h (_ :@ t) x         = h t (succ x)
 
--- test
-n0 = asNat $ Lam c0
-n1 = asNat $ Lam c1
 
--- mod lambda calc codec end
+-- THE MACHINE
 
 type S = (Exp, Env, Kont)
 stateControl :: S -> Exp
 stateControl s = c where (c, _, _) = s
 
 step :: S -> S
+
+-- Evaluating a reference? Look it up in the environment.
+-- Evaluating a function application? First evaluate the function.
+-- Evaluated the function? Go evaluate the argument term.
+-- Evaluated the argument too? Perform the application.
 
 step (Ref x, r, k)
    = (Lam v, r', k) where Clo (v, r') = r(x) 
@@ -59,24 +66,42 @@ step (Lam v, r, Ar(e, r', k))
    = (e, r', Fn(v, r, k))
 
 step (Lam v, r, Fn(x :=> e, r', k))
-   = (e, r' // [x ==> Clo(v, r)], k) where
-        -- asTuple syntax.
-        (==>) :: a -> b -> (a, b)
-        (==>) x y = (x, y)
-        -- Anticipate Fn's x. In x case return the anticipated y, otherwise inquire r'.
-        (//) :: Eq a => (a -> b) -> [(a, b)] -> (a -> b)
-        (//) r' [(x, y)] = \ input -> if (x == input) then y else r'(input)
+   = (e, r' // [x ==> Clo(v, r)], k)
+   where
+           -- "as tuple" syntax.
+           (==>) :: a -> b -> (a, b)
+           (==>) x y = (x, y)
+
+           -- In x case return the anticipated y, otherwise inquire r'.
+           (//) :: Eq a => (a -> b) -> [(a, b)] -> (a -> b)
+           (//) r' [(x, y)] = \ input -> if (x == input) then y else r'(input)
+
+unchurch :: S -> Int
+unchurch s = asNat $ stateControl s
+
+inject :: Exp -> S
+inject (e) = (e, defaultEnv, Mt)
+
+isFinal :: S -> Bool
+isFinal (Lam _, _, Mt) = True
+isFinal _ = False
+
+eval :: S -> S
+eval = until isFinal step 
 
 
-s0 = (Ref "not_in_env", defaultEnv, Mt)
+-- TESTING
+
+s0 = inject (Ref "not_in_env")
+-- ghci> stateControl $ eval s0
+-- Lam ("" :=> Ref "not_in_env")
 
 r1 :: Env
-r1 "one" = Clo (c1, defaultEnv)
-s1 = (Ref "one", r1, Mt)
+r1 "eleven" = Clo (asChurchNum 11, defaultEnv)
+s1 = (Ref "eleven", r1, Mt)
+-- ghci> unchurch $ eval s1
+-- 11
 
-s2 = (Lam c1, defaultEnv, Mt)
--- asNat $ stateControl s2
-
--- s3 = (app, defaultEnv, Mt) where app = Lam ident :@ Lam c1
-s3 = (app, defaultEnv, Mt) where app = Lam ident :@ Lam c0
--- asNat $ stateControl $ step $ step s3
+s2 = inject (Lam ident :@ (Lam $ asChurchNum 5))
+-- ghci> unchurch $ eval s2
+-- 5

790102091c4f5194a41fe93a7c662a0107701625
Reza <rezahandzalah@gmail.com>
2023-10-26 15:50:11 +0700

    some steps

--- cesk/src/Lib.hs
+++ cesk/src/Lib.hs
@@ -3,105 +3,100 @@
 
 module Lib where
 
-data Exp = Ref Var
-         | Lam Lambda
-         | Exp :@ Exp
+-- LANGUAGE: A-NORMAL FORM
+
+data AExp = ALambda Lambda | AVar Var | ABool Bool | AInt Int
+        deriving Show
+
+data CExp = If (AExp, Exp, Exp)
+        deriving Show
+
+data Exp = Atomic AExp
+         | Complex CExp
+         | Let (Var, Exp, Exp)
          deriving Show
 
-type Env = Var -> D
-instance Show Env where show _ = "Env"
-defaultEnv :: Env
-defaultEnv var = Clo (thunk, \x -> defaultD) where thunk = "" :=> Ref var
-
-data Kont = Mt
-          | Ar (Exp, Env, Kont)
-          | Fn (Lambda, Env, Kont)
-          deriving Show
-
 type Var = String
-
-data D = Clo (Lambda, Env) deriving Show
-defaultD = Clo (asChurchNum 0, defaultEnv)
 
 data Lambda = Var :=> Exp
         deriving Show
 
 
--- CHURCH ENCODING
+-- asChurchNum :: Int -> Lambda
+-- asChurchNum n = "f" :=> Lam ("x" :=> (tally n)) where tally :: Int -> Exp
+--                                                       tally 0 = Ref "x"
+--                                                       tally n = (Ref "f") :@ (tally $ pred n)
+-- asNat :: Exp -> Int
+-- asNat e = h e 0 where
+--         h (Ref _) x          = x
+--         h (Lam (_ :=> t)) x  = h t x
+--         h (_ :@ t) x         = h t (succ x)
 
-ident = "x" :=> Ref "x"
+-- Store extension σ[a↦value]  ??
+type Store = Addr -> Value
+data Kont = Letk (Var, Exp, Env, Kont) | Halt
+        deriving Show
 
-asChurchNum :: Int -> Lambda
-asChurchNum n = "f" :=> Lam ("x" :=> (tally n)) where tally :: Int -> Exp
-                                                      tally 0 = Ref "x"
-                                                      tally n = (Ref "f") :@ (tally $ pred n)
+data Value = ValueVoid | ValueZ Int | ValueTrue | ValueFalse | ValueKont Kont | ValueClo (Lambda, Env)
+type Addr = Int
 
-asNat :: Exp -> Int
-asNat e = h e 0 where
-        h (Ref _) x          = x
-        h (Lam (_ :=> t)) x  = h t x
-        h (_ :@ t) x         = h t (succ x)
+-- Env
+-- "as tuple" syntax.
+(==>) :: a -> b -> (a, b)
+(==>) x y = (x, y)
+-- In x case return the anticipated y, otherwise inquire fn.
+(//) :: Eq a => (a -> b) -> [(a, b)] -> (a -> b)
+(//) fn [(x, y)] = \ input -> if (x == input) then y else fn(input)
 
 
 -- THE MACHINE
 
-type S = (Exp, Env, Kont)
+type S = (Exp, Env, Store, Kont)
 stateControl :: S -> Exp
-stateControl s = c where (c, _, _) = s
+stateControl s = c where (c, _, _, _) = s
+
+stateFrom :: (Kont, Value, Store) -> S  -- applykont
+stateFrom (Letk (v, e, r, k), value, t)
+            = (e, r // [v ==> a], t // [a ==> value], k) where a = 1  -- ??
+
+type Env = Var -> Addr
+instance Show Env where show _ = "Env"
+
+atomic :: (AExp, Env, Store) -> Value
+atomic (AVar v, r, t)     = t $ r v
+atomic (AInt z, _, _)     = ValueZ z
+atomic (ABool b, _, _)    = if b then ValueTrue else ValueFalse
+atomic (ALambda l, r, _)  = ValueClo (l, r)
+-- Primitive expressions ??
 
 step :: S -> S
 
--- Evaluating a reference? Look it up in the environment.
--- Evaluating a function application? First evaluate the function.
--- Evaluated the function? Go evaluate the argument term.
--- Evaluated the argument too? Perform the application.
+step (Atomic aexp, r, t, k) = stateFrom (k, atomic (aexp, r, t), t)
 
-step (Ref x, r, k)
-   = (Lam v, r', k) where Clo (v, r') = r(x) 
+step (Let (v, exp, body), r, t, k)
+   = (exp, r, t, Letk (v, body, r, k))
 
-step (f :@ e, r, k)
-   = (f, r, Ar(e, r, k))
+step (Complex (If (aexp, tExp, fExp)), r, t, k)
+   = case atomic (aexp, r, t) of
+           ValueFalse  -> (fExp, r, t, k)
+           _           -> (tExp, r, t, k)
 
-step (Lam v, r, Ar(e, r', k))
-   = (e, r', Fn(v, r, k))
-
-step (Lam v, r, Fn(x :=> e, r', k))
-   = (e, r' // [x ==> Clo(v, r)], k)
-   where
-           -- "as tuple" syntax.
-           (==>) :: a -> b -> (a, b)
-           (==>) x y = (x, y)
-
-           -- In x case return the anticipated y, otherwise inquire r'.
-           (//) :: Eq a => (a -> b) -> [(a, b)] -> (a -> b)
-           (//) r' [(x, y)] = \ input -> if (x == input) then y else r'(input)
-
-unchurch :: S -> Int
-unchurch s = asNat $ stateControl s
-
-inject :: Exp -> S
-inject (e) = (e, defaultEnv, Mt)
-
-isFinal :: S -> Bool
-isFinal (Lam _, _, Mt) = True
-isFinal _ = False
-
-eval :: S -> S
-eval = until isFinal step 
-
-
--- TESTING
-
-s0 = inject (Ref "not_in_env")
--- ghci> stateControl $ eval s0
--- Lam ("" :=> Ref "not_in_env")
-
-r1 :: Env
-r1 "eleven" = Clo (asChurchNum 11, defaultEnv)
-s1 = (Ref "eleven", r1, Mt)
--- ghci> unchurch $ eval s1
--- 11
-
-s2 = inject (Lam ident :@ (Lam $ asChurchNum 5))
--- ghci> unchurch $ eval s2
--- 5
+-- unchurch :: S -> Int
+-- unchurch s = asNat $ stateControl s
+--
+-- inject :: Exp -> S
+-- inject (e) = (e, defaultEnv, Mt)
+--
+-- isFinal :: S -> Bool
+-- isFinal (Lam _, _, Mt) = True
+-- isFinal _ = False
+--
+-- eval :: S -> S
+-- eval = until isFinal step 
+--
+--
+-- -- TESTING
+--
+-- s0 = inject (Ref "not_in_env")
+-- -- ghci> stateControl $ eval s0
+-- -- Lam ("" :=> Ref "not_in_env")

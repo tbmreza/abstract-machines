38509517f2a785b07bf0a3a2980847d775f03b5a
Reza <rezahandzalah@gmail.com>
2023-10-30 21:28:22 +0700

    readkont Halt ok

--- cesk/src/Lib.hs
+++ cesk/src/Lib.hs
@@ -5,19 +5,31 @@ module Lib where
 
 -- LANGUAGE: A-NORMAL FORM
 
-data AExp = ALambda Lambda | AVar Var | ABool Bool | AInt Int | APrimAppl Prim [AExp]
+data AExp = ALambda Lambda | AVar Var
+          | ABool Bool | AInt Int
+          | APrimAppl Prim [AExp]
+        -- deriving Show
 data Prim = Add | Sub | Mul | Eq
+        deriving Show
 
 instance Show AExp where
         show (ALambda d) = show d
+        -- show (AVar d) = atomic d r t ...
         show (AVar d) = show d
         show (ABool d) = if d then "#t" else "#f"
         show (AInt d) = show d
         show a@(APrimAppl _ d) = show $ atomic (a, defaultEnv, defaultStore)
-
         show _ = "AExp todo.."
         -- `show (_ d) = show d` if I could but
         -- wildcard in data constructor's place is not allowed.
+
+data Value = Undefined | ValueVoid | ValueZ Int | ValueTrue | ValueFalse
+           | ValueKont Kont | ValueClo (Lambda, Env)
+        -- deriving Show -- ??
+instance Show Value where
+        show Undefined = show Undefined
+        show (ValueZ d) = show d
+        show _ = "Value"
 
 data CExp = If      AExp Exp Exp
           | Set     Var AExp
@@ -33,17 +45,17 @@ data Exp = Atomic   AExp
 
 instance Show Exp where
         show (Atomic d) = show d
-        show _ = "Exp todo"
+        show (Complex d) = show d
+        show d = show d
 
-defaultExp = Atomic $ AInt 0
+defaultExp = Atomic $ ABool False
 
 type Var = String
 
-data Lambda = Var :=> Exp
+data Lambda = [Var] ::=> Exp
         deriving Show
-
-data MLambda = [Var] ::=> Exp
-        deriving Show
+ident :: Lambda
+ident = ["x"] ::=> Atomic (AVar "x")
 
 data Bind = Var := AExp
         deriving Show
@@ -60,10 +72,6 @@ defaultStore _ = Undefined
 data Kont = Letk Var Exp Env Kont | Halt
         deriving Show
 
-data Value = Undefined | ValueVoid | ValueZ Int | ValueTrue | ValueFalse
-           | ValueKont Kont | ValueClo (Lambda, Env) | ValueCloM (MLambda, Env)
-        -- ?? instance Show, some in AExp maybe redundant
-        deriving Show
 type Addr = Int
 
 -- Env and Store ?? typeclass
@@ -78,20 +86,31 @@ type Addr = Int
 -- THE MACHINE
 
 type S = (Exp, Env, Store, Kont)
+defaultS = (defaultExp, defaultEnv, defaultStore, Halt)
 stateControl :: S -> Exp
 stateControl s = c where (c, _, _, _) = s
 
 applyproc :: (Value, [Value], Store, Kont) -> S
-applyproc (ValueCloM (vs ::=> body, r), args, t, k)
-      = (body, r, t, k)
+applyproc (ValueClo (vars ::=> body, r), args, t, k) = (body, r', t', k) where
+        addrs = alloc (length vars) t
+        r' = foldl rExtend r $ zip vars addrs
+        t' = foldl tExtend t $ zip addrs args
 
-readKont :: (Kont, Value, Store) -> S  -- applykont
--- readKont (Halt, _, _) = error "reading Halt continuation"
-readKont ((Letk v e r k), value, t)
-       = (e, r // [v ==> a], t // [a ==> value], k) where
-       a = freshAddr 0
-       freshAddr :: Addr -> Addr
-       freshAddr addr = case t addr of
+applyproc _ = error "applying non-procedure Value"
+
+readkont :: (Kont, Value, Store) -> S  -- applykont
+readkont (Halt, value, _) = (Atomic ae, defaultEnv, defaultStore, Halt) where
+        ae :: AExp
+        ae = case value of
+                ValueZ d -> AInt d
+                ValueTrue -> ABool True
+                ValueFalse -> ABool False
+                _ -> AInt 0  -- ??
+
+readkont ((Letk v e r k), value, t) = (e, r // [v ==> a], t // [a ==> value], k) where
+        a = freshAddr 0
+        freshAddr :: Addr -> Addr
+        freshAddr addr = case t addr of
                 Undefined  -> addr
                 _          -> freshAddr $ succ addr
 
@@ -102,9 +121,9 @@ defaultEnv _ = error "Addr undefined in Env for input Var"
 instance Show Env where show _ = "Env"
 
 atomic :: (AExp, Env, Store) -> Value
-atomic (AVar v, r, t)     = t $ r v
-atomic (AInt z, _, _)     = ValueZ z
-atomic (ABool b, _, _)    = if b then ValueTrue else ValueFalse
+atomic (AVar v, r, t) = t $ r v
+atomic (AInt z, _, _) = ValueZ z
+atomic (ABool b, _, _) = if b then ValueTrue else ValueFalse
 
 atomic (APrimAppl op aexps, r, t) = ValueZ z where
         fnadd :: AExp -> AExp -> AExp  -- ?? template haskell?
@@ -114,26 +133,26 @@ atomic (APrimAppl op aexps, r, t) = ValueZ z where
         fnsub a b = AInt new where
                 [AInt acc, AInt arg] = [a, b]
                 new = acc - arg
+        fnmul a b = AInt new where
+                [AInt acc, AInt arg] = [a, b]
+                new = acc * arg
 
         -- Map primitive (prefix) ops to host language's (infix) ops.
         AInt z = case op of
                 Add -> foldl fnadd (head aexps) (tail aexps)
                 Sub -> foldl fnsub (head aexps) (tail aexps)
+                Mul -> foldl fnmul (head aexps) (tail aexps)
                 -- ??
 
-atomic _     = ValueVoid
--- atomic (ALambda l, r, _)  = ValueClo (l, r)  ?? MLambda
-
--- test ok: atomic a1
--- a1 = (APrimAppl Add [AInt 2, AInt 3], defaultEnv, defaultStore)
--- a2 = (APrimAppl Sub [AInt 7, AInt 1], defaultEnv, defaultStore)
+atomic (ALambda l, r, _) = ValueClo (l, r)
+atomic _ = ValueVoid
 
 step :: S -> S
 
-step (Atomic aexp, r, t, k) = readKont (k, atomic (aexp, r, t), t)
+step (Atomic aexp, r, t, k) = readkont (k, atomic (aexp, r, t), t)
 
-step ((Let v exp body), r, t, k)
-   = (exp, r, t, (Letk v body r k))
+step ((Let var exp body), r, t, k)
+   = (exp, r, t, (Letk var body r k))
 
 step (Complex (If aexp tExp fExp), r, t, k)
    = case atomic (aexp, r, t) of
@@ -141,16 +160,14 @@ step (Complex (If aexp tExp fExp), r, t, k)
            _           -> (tExp, r, t, k)
 
 step (Complex (Set v aexp), r, t, k)
-   = readKont (k, ValueVoid, t // [r v ==> atomic (aexp, r, t)])
+   = readkont (k, ValueVoid, t // [r v ==> atomic (aexp, r, t)])
 
-step (Complex (Proc exps), r, t, k)
+step (Complex (Proc aexps), r, t, k)
    = applyproc (closure, args, t, k)
    where
-        ai :: AExp -> Value
-        ai arg = atomic (arg, r, t)
-        -- [closure, args] = map ai $ exps  ?? split hd tail
-        closure = ai $ head exps
-        args = map ai $ tail exps
+        asValue :: AExp -> Value
+        asValue arg = atomic (arg, r, t)
+        closure : args = map asValue aexps
 
 step (Complex (Letrec binds body), r, t, k)
    = (body, r', t', k)
@@ -187,13 +204,6 @@ alloc cap store = addrs where
                 _          -> h (succ addr) acc
         addrs = h 0 []
 
--- test ok:
--- st1 :: Store
--- st1 1 = ValueTrue
--- st1 addr = defaultStore addr
--- teste = alloc 3 defaultStore
--- testd = alloc 3 st1
-
 rExtend :: Env -> (Var, Addr) -> Var -> Addr
 rExtend r (var, addr) = r' where
         r' var = addr
@@ -207,20 +217,40 @@ tExtend t (addr, val) = t' where
 inject :: Exp -> S
 inject (e) = (e, defaultEnv, defaultStore, Halt)
 
+injectWith :: Env -> Store -> Exp -> S
+injectWith r t (e) = (e, r, t, Halt)
+
 eval :: S -> S
 eval = until isFinal step 
 
 isFinal :: S -> Bool
-isFinal (_, _, _, Halt) = True
+isFinal (Atomic (AVar _), _, _, Halt) = False
+isFinal (Atomic _, _, _, Halt) = True
 isFinal _ = False
 
 
 -- TESTING
+-- ghci> stateControl $ eval s
 
 s0 = inject (Atomic (ABool False))
--- ghci> stateControl $ eval s0
 -- #f
 
 s1 = inject (Atomic (APrimAppl Add [AInt 2, AInt 13]))
--- ghci> stateControl $ eval s1
--- ValueZ 15
+-- 15
+
+[s2, s3] = [
+        inject (Complex (If (ABool True) (Atomic $ AInt 3) (Atomic $ AInt 2))),
+        inject (Complex (If (ABool False) (Atomic $ AInt 3) (Atomic $ AInt 2))) ]
+-- [3,2]
+
+s4 = inject (Complex (Proc aexps)) where aexps = [ALambda ident, AInt 4]
+-- 4
+
+s5 = injectWith r5 t5 (Atomic $ AVar "b") where
+        r5 :: Env
+        r5 "b" = 12
+        t5 :: Store
+        t5 12 = ValueZ 6
+-- 6
+
+testAll = map stateControl (map eval [s0, s1, s2, s3, s4, s5])

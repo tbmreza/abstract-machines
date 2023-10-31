49bbbb71b0b070f66763a474967bc7f15831fd2d
Reza <rezahandzalah@gmail.com>
2023-11-01 01:30:48 +0700

    isFinal fn

--- cesk-star/main.rkt
+++ cesk-star/main.rkt
@@ -18,11 +18,30 @@
   (or (any? v)     ; Kont
       (cons? v)))  ; denotable values (val . env)
 
+(define (Kont? v) (list? v))  ; mt | ar(e,r,a) | fn(l,r,a)
+
 (struct S (expr env store addr) #:transparent)
+(define mt 0)
+(define default-addr 0)
 (define default-env (hash))
-; (define default-store (hash))  0 mt
+(define default-store (hash default-addr mt))
 
 (define/contract (inj e) (-> Exp? struct?)
-  (S e (hash) 3 4))
+  (S e default-env default-store default-addr))
 
-(inj add1)
+; (inj add1)
+
+
+; THE MACHINE
+
+(define/contract (final? s) (-> struct? boolean?)
+  (match s
+    [(S e _r t a) #:when
+                  (and (procedure? e)
+                       (empty? (hash-ref t a #f))) true]
+    [_ false]))
+
+(define str (hash 1001 (list)))
+
+(final? (S add1 default-env default-store 1001))
+(final? (S add1 default-env str 1001))

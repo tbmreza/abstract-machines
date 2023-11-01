021428ede7b1ea4d9c7b362ce15bd4a86c1f0b7b
Reza <rezahandzalah@gmail.com>
2023-11-01 19:57:30 +0700

    church numeral

--- cesk-star/main.rkt
+++ cesk-star/main.rkt
@@ -7,11 +7,14 @@
 
 interpreting:  lambda calculus
 using:         CESK* machine (tick/alloc: [X] none  [ ] functions  [ ] stacks)
+               fig.3 of Abstracting Abstract Machines paper
 
 observations I'm not terribly sure about:
 - Kont is also Storable along with Value(Env); atomic evaluation can yield Kont
 - the adj "abstract" in abstract time-stamped cesk* means we use stack data structures
   in tick and alloc instead of first-class functions
+- https://smlhelp.github.io/book/docs/concepts/control-flow/cps
+  will provide extra context after implementing second def
 
 |#
 
@@ -24,18 +27,43 @@ observations I'm not terribly sure about:
 
 (define (Storable? v)
   (or (any? v)     ; Kont
-      (cons? v)))  ; denotable values (val . env)
+      (cons? v)))  ; denotable values (lam . env)
+
+(define (Store? v) (hash? v))  ; Map Addr Storable?
+(define/contract (lookup m k) (-> hash? any? any?)
+  ; Panicking is desired behavior.
+  (hash-ref m k))
 
 (define (Kont? v) (list? v))  ; mt | ar(e,r,a) | fn(l,r,a)
+(define mt (list))
 
 (struct S (expr env store addr) #:transparent)
-(define mt (list))
 (define default-addr 0)
 (define default-env (hash))
 (define default-store (hash default-addr mt))
 
 (define/contract (inj e) (-> Exp? struct?)
   (S e default-env default-store default-addr))
+
+(define (inj-with r σ e)
+  (S e r σ default-addr))
+
+; CHURCH ENCODING
+
+(define ident (lambda (x) x))
+(define c1 (lambda (f) (lambda (x) (f x))))
+(define c2 (lambda (f) (lambda (x) (f (f x)))))
+
+(define (c->number c)
+  ((c add1) 0))
+(check-eq? (c->number c2) 2)
+
+(define (cnat n)
+  (define (h n acc)
+    (match n
+      [0 acc]
+      [_ `(f ,(h (sub1 n) acc))]))
+  (eval `(lambda (f) (lambda (x) ,(h n 'x))) (make-base-namespace)))
 
 
 ; THE MACHINE
@@ -59,26 +87,51 @@ observations I'm not terribly sure about:
       [_        (go (f x))]))
   go)
 
+(define/contract (fresh-addr store) (-> hash? number?)
+  (define (h addr)
+    (match (lookup store addr)
+      [#f  addr]
+      [_   (h (add1 addr))]))
+  (h 0))
+
+(define (extend m addr storable)
+  (hash-set m addr storable))
+
 (define/contract (step s) (-> struct? struct?)
-  s)
+  (match s
+    [(S (? string? e) r σ a)
+     (let* ([e-addr  (lookup r e)]
+            [d       (lookup σ e-addr)])
+       (S (car d) (cdr d) σ a))]
+
+    [(S (? cons? e) r σ a)
+     (let* ([e0  (car e)]
+            [e1  (cdr e)]
+            [b   (fresh-addr σ)]
+            [σ/  (extend σ b (list e1 r a))])  ; ?? struct ar fn
+       (S e0 r σ/ b))]
+
+    [_ s]))
+
+; ; test ok:  ?? unit
+; (let ([r   (hash "x" 1001)]
+;       [to  (hash 1001 (cons c1 default-env))])
+;       (step (inj-with r to "x")))
+
 
 (define/contract (eval s) (-> struct? struct?)
   ((until final? step) s))
 
+
 ; -- TESTING
 
 (check-equal?
-  "not_in_env"  ; ?? "(lambda () not_in_env)"
   (let* ([start  (inj "not_in_env")]
          [final  (eval start)])
-    (S-expr final)))  ; ?? define control-str
+    (S-expr final))  ; ?? define control-str
+  "not_in_env")  ; ?? expect thunk (lambda () not_in_env)
 
-; r1 :: Env
-; r1 "eleven" = Clo (asChurchNum 11, defaultEnv)
-; s1 = (Ref "eleven", r1, Mt)
-; -- ghci> unchurch $ eval s1
-; -- 11
-
-; s2 = inject (Lam ident :@ (Lam $ asChurchNum 5))
-; -- ghci> unchurch $ eval s2
-; -- 5
+(let* ([start  (inj (cons ident c2))]
+       [final  (eval start)])
+  (display 'final:)(displayln final)
+  (S-expr final))

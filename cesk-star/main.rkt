b6a06b3cd3d902f178a14de99699bbb301afc2cc
Reza <rezahandzalah@gmail.com>
2023-11-01 11:04:32 +0700

    sets up first step case

--- cesk-star/main.rkt
+++ cesk-star/main.rkt
@@ -3,11 +3,12 @@
 (provide (all-defined-out))
 (require rackunit)
 
-; interpreting:  lambda calculus
-; using:         CESK* machine (tick/alloc: [X] none  [ ] functions  [ ] stacks)
+#|
 
-#| observations I'm not terribly sure about:
+interpreting:  lambda calculus
+using:         CESK* machine (tick/alloc: [X] none  [ ] functions  [ ] stacks)
 
+observations I'm not terribly sure about:
 - Kont is also Storable along with Value(Env); atomic evaluation can yield Kont
 - the adj "abstract" in abstract time-stamped cesk* means we use stack data structures
   in tick and alloc instead of first-class functions
@@ -36,8 +37,6 @@
 (define/contract (inj e) (-> Exp? struct?)
   (S e default-env default-store default-addr))
 
-; (inj add1)
-
 
 ; THE MACHINE
 
@@ -46,7 +45,40 @@
     [(S e _r σ a) #:when
                   (and (procedure? e)
                        (empty? (hash-ref σ a #f))) true]
-    [_ false]))
-(check-false (final? (S add1 default-env default-store 1001)))
-(check-true
-    (let ([σ (hash 1001 (list))]) (final? (S add1 default-env σ 1001))))
+    ; [_ false]))
+    [_ true]))
+; dont commit
+; (check-false (final? (S add1 default-env default-store 1001)))
+; (check-true
+;     (let ([σ (hash 1001 (list))]) (final? (S add1 default-env σ 1001))))
+
+(define (until p f)
+  (define (go x)
+    (match x
+      [(? p x)  x]
+      [_        (go (f x))]))
+  go)
+
+(define/contract (step s) (-> struct? struct?)
+  s)
+
+(define/contract (eval s) (-> struct? struct?)
+  ((until final? step) s))
+
+; -- TESTING
+
+(check-equal?
+  "not_in_env"  ; ?? "(lambda () not_in_env)"
+  (let* ([start  (inj "not_in_env")]
+         [final  (eval start)])
+    (S-expr final)))  ; ?? define control-str
+
+; r1 :: Env
+; r1 "eleven" = Clo (asChurchNum 11, defaultEnv)
+; s1 = (Ref "eleven", r1, Mt)
+; -- ghci> unchurch $ eval s1
+; -- 11
+
+; s2 = inject (Lam ident :@ (Lam $ asChurchNum 5))
+; -- ghci> unchurch $ eval s2
+; -- 5

387c8c9b1d3ee28e1bf2c0da254682da8a19270d
Reza <rezahandzalah@gmail.com>
2023-11-03 15:04:30 +0700

    sets up step debugging

--- cesk-star-timestamp/main.rkt
+++ cesk-star-timestamp/main.rkt
@@ -58,6 +58,13 @@
 (define (extend m k v)
   (hash-set m k v))
 
+(struct time (z) #:transparent)
+(struct addr (z) #:transparent)
+
+(define/contract (tick state) (-> S? time?)
+  (time (add1 (S-time state))))
+(define/contract (alloc state) (-> S? addr?)
+  (addr (S-time state)))
 ; (define (fresh-addr store)
 ;   (define (h addr)
 ;     (match (lookup store addr)
@@ -65,48 +72,54 @@
 ;       [_   (h (add1 addr))]))
 ;   (h 0))
 
-; (struct decapped (var body) #:transparent)
-; (define (decap lxe)
-;   (match lxe
-;     [`(lambda (,x) ,e)
-;       (define xe-string-pair (map (lambda (v) (format "~a" v))
-;                                   (list x e)))
-;       (apply decapped xe-string-pair)]))
+(struct decapped (var body) #:transparent)
+(define (decap lxe)
+  (match lxe
+    [`(lambda (,x) ,e)
+      (define xe-string-pair (map (lambda (v) (format "~a" v))
+                                  (list x e)))
+      (apply decapped xe-string-pair)]))
 
 (define (step s)
-  s
-  ; (define state (match s
-  ;   [(S (? variable-name? e) r σ a)
-  ;    (let* ([e-addr  (lookup r e)]
-  ;           [d       (lookup σ e-addr)])
-  ;      (S (clo-lam d) (clo-env d) σ a))]
-  ;
-  ;   [(S (? app? e) r σ a)
-  ;    (let* ([e0  (app-e0 e)]
-  ;           [e1  (app-e1 e)]
-  ;           [b   (fresh-addr σ)]
-  ;           [k   (ar e1 r a)]
-  ;           [σ+  (extend σ b k)])
-  ;      (S e0 r σ+ b))]
-  ;
-  ;   [(S v r σ a)
-  ;    (match (lookup σ a)
-  ;      [(ar e r+ c)
-  ;       (let* ([b   (fresh-addr σ)]
-  ;              [k   (fn v r c)]
-  ;              [σ+  (extend σ b k)])
-  ;         (S e r+ σ+ b))]
-  ;
-  ;      [(fn l r+ c)
-  ;       (let* ([xe   (decap l)]
-  ;              [x    (decapped-var xe)]
-  ;              [e    (decapped-body xe)]
-  ;              [b    (fresh-addr σ)]
-  ;              [r++  (extend r+ x b)]
-  ;              [σ+   (extend σ b (clo v r))])
-  ;         (S e r++ σ+ c))])]))
-  ; ; (displayln "\nstate:")(displayln state)
-  ; state
+  (define state (match s
+    [(S (? variable-name? e) r σ a t)
+     (let* ([e-addr  (lookup r e)]
+            [d       (lookup σ e-addr)]
+            [u       (tick t)])
+       (S (clo-lam d) (clo-env d) σ a u))]
+
+    [(S (? app? e) r σ a t)
+     (let* ([e0  (app-e0 e)]
+            [e1  (app-e1 e)]
+            [b   (alloc s)]
+            [k   (ar e1 r a)]
+            [σ+  (extend σ b k)]
+            [u   (tick t)])
+       (S e0 r σ+ b u))]
+
+    [(S v r σ a t)
+     (match (lookup σ a)
+       [(ar e r+ c)
+        (let* ([b   (alloc s)]
+               [k   (fn v r c)]
+               [σ+  (extend σ b k)]
+               [u   (tick t)])
+          (S e r+ σ+ b u))]
+
+       [(fn l r+ c)
+        (let* ([xe   (decap l)]
+               [x    (decapped-var xe)]
+               [e    (decapped-body xe)]
+               [b    (alloc s)]
+               [r++  (extend r+ x b)]
+               [σ+   (extend σ b (clo v r))]
+               [u   (tick t)])
+          (S e r++ σ+ c u))])]
+    
+    ))
+  ; s
+  ; (displayln "\nstate:")(displayln state)
+  state
   )
 
 (define (until p f)
@@ -135,23 +148,20 @@
             [var-value  (eval-clo var-d)])
        (c->number var-value))]
 
-    ; [(S (? church-encoded? e) _r _σ _a)
-    ;  (c->number e)]
-    ;
-    ; [(S e _r _σ _a)
-    ;  (format "~a" e)]
-    )
-  ; (void)
-  )
+    [(S (? church-encoded? e) _r _σ _a _t)
+     (c->number e)]
+
+    [(S e _r _σ _a _t)
+     (format "~a" e)]))
 
 
 ; -- TESTING
 
-; (check-equal?
-;   (let* ([start  (inj "not_in_env")]
-;          [final  (run start)])
-;     (interpret-control-str final))
-;   "not_in_env")
+(check-equal?
+  (let* ([start  (inj "not_in_env")]
+         [final  (run start)])
+    (interpret-control-str final))
+  "not_in_env")
 
 (check-equal?
   (let* ([r      (hash "x" 1001)]
@@ -161,12 +171,15 @@
     (interpret-control-str final))
   2)
 
-; (check-eq?
+; ; (check-eq?
 ;   (let* ([start  (inj (app ident (cnat 11)))]
-;          [final  (run start)])
-;     (interpret-control-str final))
-;   11)
-;
+;          [fwd  (step start)]
+;          ; [final  (run start)]
+;          )
+;     ; (interpret-control-str final)
+;     (void))
+; ;   11)
+
 ; (check-eq?
 ;   (let* ([start  (inj (app ident (app ident (app ident (cnat 21)))))]
 ;          [final  (run start)])

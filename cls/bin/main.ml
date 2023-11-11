83d05c37705e1b50139ec508df5d0a1e075590ac
Reza <rezahandzalah@gmail.com>
2023-11-11 11:41:44 +0700

    step 3

--- cls/bin/main.ml
+++ cls/bin/main.ml
@@ -11,10 +11,15 @@ type term = Ind of int
           (* | App of term * term *)
 let ident = Abs (Ind 0)
 
+(* (lambda (f) (lambda (x) x)) *)
+(* (lambda (f) (lambda (x) (f x))) *)
+
 (* instruction = term | ap *)
-type env = int
-type value = Closure of term * env
-let _v = Closure (ident, 0)
+
+type env = Env of value list
+and value = Clo of term * env
+
+let _v = Clo (ident, Env [])
 
 type c = term list
 type l = env list
@@ -26,6 +31,14 @@ let _s = State ([], [], [])
 let inj t = State (t :: [], [], [])
 
 let initial: state = inj ident
+
+(* STEPS *)
+(* 3. Ident, pop l, push its term to s *)
+
+let _step = function
+        | State ((Ind 0) :: c, (Env (v :: _)) :: l, s) -> State (c, l, v :: s) (* 3 *)
+
+        | _ -> assert false
 
 let unload = function
         | State ([], [], v :: _) -> Some v

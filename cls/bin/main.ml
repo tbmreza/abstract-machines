21d843ffeab48f010376a76bb267170675847e90
Reza <rezahandzalah@gmail.com>
2023-11-11 15:54:26 +0700

    AP instr and comments

--- cls/bin/main.ml
+++ cls/bin/main.ml
@@ -1,6 +1,6 @@
 (* interpreting:  lambda calculus (de bruijn notation)
    using:         CLS machine ([X] original [ ] disentangled)
-                  fig.21 of Han92 "From Operational Semantics to Abstract Machines" paper
+                  fig.1 of Han91 "Staging transformations for abstract machines" paper
 
                   C ontrol (list of instructions)
                   L ist of environments
@@ -15,7 +15,6 @@ type term = Ind of int
           | Abs of term
           | App of term * term
 let ident = Abs (Ind 0)
-let ap = ident (* ?? *)
 let _a = App (ident, ident)
 
 (* DE BRUIJN NOTATION ENCODED EXPRESSIONS *)
@@ -29,14 +28,15 @@ let _a = App (ident, ident)
 (* let _c3 = Abs (App ((Ind 1), App ((Ind 1), App ((Ind 1), (Ind 0))))) *)
 let _c3 = Abs (Abs (App ((Ind 1), App ((Ind 1), App ((Ind 1), (Ind 0))))))  (* ?? as_int *)
 
-(* instruction = term | ap *)
+type instr = Term of term
+           | AP  (* special instruction that "moves the computation back to the compilation section." *)
 
 type env = Env of value list
 and value = Clo of term * env
 
 let _v = Clo (ident, Env [])
 
-type c = term list
+type c = instr list
 type l = env list
 type s = value list
 
@@ -45,39 +45,41 @@ let _s = State ([], [], [])
 
 let inj t = State (t :: [], [], [])
 
-let initial: state = inj ident
+let initial: state = inj (Term ident)
 
 (* STEPS
 
-First describing the algorithm poorly:
-  3. index closest to binder; pop l; push its term to s
-  4. index n, pred n; pop l, push its tail back to l; s unchanged
-  1. abs head, control tail; pop l; the 2 combine for new s head
-  2. app head, reform control with ap sequence; l pushes its own head; s unchanged
-  5. ap labeled, control (second s) term; push l with that term * env; (cddr s)
+First spelling out the match cases poorly:
+  a. abs head, control tail; pop l; the 2 combine for new s head
+  b. app head, reform control with ap sequence; l pushes its own head; s unchanged
+  c. index closest to binder; pop l; push its term to s
+  d. index n, pred n; pop l, push its tail back to l; s unchanged
+  e. ap labeled, control (second s) term; push l with that term * env; (cddr s)
 
 Now for intuition:
-  3. 0 means index is closest to its lambda binder. Topmost value in the l can jump to s.
-  4. With base condition out of the way, now we look to pred index n. We'll handle the topmost value in the base case, so we have to pop it for now.
-  ...
-
+  a. Abstraction term means that it can jump together with the topmost env to s.
+  b. We spread application of terms as its constituents followed with special AP as a marker.
+     Now if the state that we entered this step with has e as topmost env, in the next state that e is duplicated to accommodate for the application that we spreaded.
+  c. 0 means index is closest to its lambda binder. Topmost value in the l can jump to s.
+  d. With base condition specified, now we look to pred index n. We'll handle the topmost value in the base case, so we have to pop it for now.
+  e. AP instructs that topmost term in s jumps to c, topmost value jumps to l together with the env beneath it. This step dances with step b.
 *)
 
 let _step = function
-        | State ((Ind 0) :: c, (Env (v :: _)) :: l, s) ->
-          State (c, l, v :: s) (* 3 *)
-
-        | State ((Ind n) :: c, (Env (_ :: e)) :: l, s) ->
-          State ((Ind (pred n)) :: c, (Env e) :: l, s) (* 4 *)
-
-        | State ((Abs t) :: c, e :: l, s) ->
+        | State ((Term (Abs t)) :: c, e :: l, s) ->
           State (c, l, Clo (t, e) :: s) (* 1 *)
 
-        | State ((App (t0, t1)) :: c, e :: l, s) ->
-          State (t0 :: t1 :: ap :: c, e :: e :: l, s) (* 2 *)
+        | State ((Term (App (t0, t1))) :: c, e :: l, s) ->
+          State ((Term t0) :: (Term t1) :: AP :: c, e :: e :: l, s) (* 2 *)
 
-        | State (_ap :: c, l, v :: Clo (t, (Env e)) :: s) ->
-          State (t :: c, (Env (v :: e)) :: l, s) (* 5 *)
+        | State ((Term (Ind 0)) :: c, (Env (v :: _)) :: l, s) ->
+          State (c, l, v :: s) (* 3 *)
+
+        | State ((Term (Ind n)) :: c, (Env (_ :: e)) :: l, s) ->
+          State ((Term (Ind (pred n))) :: c, (Env e) :: l, s) (* 4 *)
+
+        | State (AP :: c, l, v :: Clo (t, (Env e)) :: s) ->
+          State ((Term t) :: c, (Env (v :: e)) :: l, s) (* 5 *)
 
         | _ -> assert false
 

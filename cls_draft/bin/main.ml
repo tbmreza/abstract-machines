abe0451b4b3503a36d00ccf8c69f258c94bf1f5a
Reza <rezahandzalah@gmail.com>
2023-11-17 17:22:59 +0700

    cp

--- cls_draft/bin/main.ml
+++ cls_draft/bin/main.ml
@@ -3,7 +3,7 @@ exception User of string
 type index = int
 type term = Ind of index
           | Lambda of term
-          (* | App of term * term *)
+          | App of term * term
 let ident = Lambda (Ind 0)
 
 type instr = Term of term
@@ -18,11 +18,11 @@ type s = addr       (* addr pointing to state's value *)
 type value = Closure of term * l
 let _v0 = Closure (Ind 0, [])
 type store = (addr -> value)
-let _default_store (a: addr) : value =
+let default_store (a: addr) : value =
         match a with
         | _ -> raise (User "accessing store with nonexistent addr")
 
-type _state = S of c * l * store * s
+type state = S of c * l * store * s
 
 
 let _step = function
@@ -34,3 +34,28 @@ let _step = function
           S (c, l, st, a)
 
         | _ -> assert false
+
+let unload = function
+        | S (_, _, st, a) -> st a
+let value_term v = match v with
+        | Closure (t, _) -> t
+(* counts number of applications of (Ind 1) *)
+let rec h c x = match (c, x) with
+        (* | ((App ((Ind 1), t)), x) -> h t (succ x) *)
+        | ((App (Ind _, t)), x) -> h t (succ x)  (* ?? *)
+        | ((Lambda t), x) -> h t x
+        | _ -> x
+let unchurch_num c = h c 0
+
+(* 1. staged unload *)
+let c3 = Lambda (Lambda (App (Ind 1, App (Ind 1, App (Ind 1, Ind 0)))))
+let staged_value = Closure (c3, [])
+
+let st (q: addr) : value =
+        match q with
+        | 1001 -> staged_value
+        | _ -> default_store q
+
+let state_value_num (s: state) : int = (unload s) |> value_term |> unchurch_num
+let staged: state = S ([], [], st, 1001)
+let () = assert (state_value_num staged == 3)
